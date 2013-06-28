5305676bfe073157a92d58890b2ce6c005345471
Chris Pick <chris@chrispick.com>
2013-06-27 21:49:30 -0400

    Add recipe and attribute to metadata.

--- metadata.rb
+++ metadata.rb
@@ -7,3 +7,10 @@ long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 depends          'apt'
 supports         'ubuntu', '= 12.04'
+
+recipe 'pam-ssh-agent-auth', 'Installs and enables pam_ssh_agent_auth'
+
+attribute 'pam-ssh-agent-auth/keys',
+  :display_name => 'Authorized Keys',
+  :description => 'Authorized keys to accept when sudo is authenticating via the ssh-agent',
+  :type => 'array'
